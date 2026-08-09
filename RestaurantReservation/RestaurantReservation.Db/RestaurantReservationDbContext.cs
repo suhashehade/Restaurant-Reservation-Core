1f2072e8d42d6db0f178b7d6b50b48737566ba3e
Suha Shehadeh <suha.shehadeh@foothillsolutions.com>
2026-08-09 14:29:05 +0300

    Create RestaurantReservationDbContext and add it to RestaurantReservation.Db

---
+++ RestaurantReservation/RestaurantReservation.Db/RestaurantReservationDbContext.cs
@@ -0,0 +1,26 @@
+﻿namespace RestaurantReservation.Db;
+using Microsoft.EntityFrameworkCore;
+public class RestaurantReservationDbContext: DbContext
+{
+  public RestaurantReservationDbContext()
+  {
+    
+  }
+  
+  public RestaurantReservationDbContext(DbContextOptions<RestaurantReservationDbContext> options)
+    : base(options)
+  {
+  }
+  
+  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+  {
+    if (!optionsBuilder.IsConfigured)
+    {
+      optionsBuilder.UseSqlServer("Server=.;Database=RestaurantReservationCore;Trusted_Connection=True;TrustServerCertificate=True;");
+    }
+  }
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    base.OnModelCreating(modelBuilder);
+  }
+}
