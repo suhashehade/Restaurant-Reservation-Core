15ba52bcfe3bb214e598e264f63fb4518288dae5
Suha Shehadeh <suha.shehadeh@foothillsolutions.com>
2026-08-11 11:53:14 +0300

    test CalculateRevenue in Restaurant Reservation

---
+++ RestaurantReservation/RestaurantReservation/Services/RestaurantService.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json;
+using RestaurantReservation.Db.Repositories;
+
+namespace RestaurantReservation.Services;
+
+public static class RestaurantService
+{
+    public static async Task GetRestaurantRevenue(int restaurantId, JsonSerializerOptions options)
+    {
+        decimal revenue = await RestaurantRepository.CalculateRestaurantRevenue(restaurantId);
+        Console.WriteLine(JsonSerializer.Serialize(revenue, options));
+    }
+}
