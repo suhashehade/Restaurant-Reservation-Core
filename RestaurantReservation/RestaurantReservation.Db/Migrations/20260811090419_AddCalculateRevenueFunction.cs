712382ae32d839c131bc9d60f27fc1110717013d
Suha Shehadeh <suha.shehadeh@foothillsolutions.com>
2026-08-11 12:07:36 +0300

    Write database function CalculateRevenue using database function with ef migrations

---
+++ RestaurantReservation/RestaurantReservation.Db/Migrations/20260811090419_AddCalculateRevenueFunction.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantReservation.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCalculateRevenueFunction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                                 CREATE FUNCTION fn_CalculateRevenue
+                                 (
+                                     @RestaurantId INT
+                                 )
+                                 RETURNS DECIMAL(18,2)
+                                 AS
+                                 BEGIN
+                                     DECLARE @TotalRevenue DECIMAL(18,2);
+
+                                     SELECT @TotalRevenue = ISNULL(SUM(o.TotalAmount), 0)
+                                     FROM Orders o
+                                     INNER JOIN Reservations r
+                                         ON o.ReservationId = r.ReservationId
+                                     WHERE r.RestaurantId = @RestaurantId;
+
+                                     RETURN @TotalRevenue;
+                                 END
+                                 """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                                 DROP FUNCTION IF EXISTS fn_CalculateRevenue;
+                                 """);
+        }
+    }
+}
---
+++ RestaurantReservation/RestaurantReservation.Db/Models/CustomerReservationResult.cs
@@ -0,0 +1,8 @@
+﻿namespace RestaurantReservation.Db.Models;
+
+public class CustomerReservationResult
+{
+    public int CustomerId { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+}
