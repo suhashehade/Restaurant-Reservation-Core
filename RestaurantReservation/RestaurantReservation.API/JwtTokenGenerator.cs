463e591296891aff78c5944c8466a9246c33aa4a
Suha Shehadeh <suha.shehadeh@foothillsolutions.com>
2026-08-17 11:42:11 +0300

    Implement JWT Authorization

---
+++ RestaurantReservation/RestaurantReservation.API/JwtTokenGenerator.cs
@@ -0,0 +1,69 @@
+﻿using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using RestaurantReservation.API.Auth;
+
+namespace RestaurantReservation.API;
+
+
+public class JwtTokenGenerator(IOptions<JwtConfig> jwtSettings)
+    {
+        private readonly JwtConfig _settings = jwtSettings.Value;
+    
+        public  string GenerateToken(string username, string password)
+        {
+            // if (username != "testUser" || password != "P@ssw0rd!")
+            //     return Unauthorized();
+            var expiresAt = DateTime.UtcNow.AddMinutes(_settings.ExpiryMinutes);
+        
+            var claims = new List<Claim>
+            {
+                new(JwtRegisteredClaimNames.Sub, username),
+                new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+        
+            var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_settings.Key));
+            var credentials = new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256);
+        
+            var descriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = expiresAt,
+                Issuer = _settings.Issuer,
+                Audience = _settings.Audience,
+                SigningCredentials = credentials
+            };
+            var handler = new JsonWebTokenHandler();
+            var token = handler.CreateToken(descriptor);
+            return token;
+        }
+
+        public async Task<bool> ValidateToken(string token)
+        {
+            var signingKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_settings.Key)
+            );
+
+            var validationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = signingKey,
+
+                ValidateIssuer = true,
+                ValidIssuer = _settings.Issuer,
+
+                ValidateAudience = true,
+                ValidAudience = _settings.Audience,
+
+                ValidateLifetime = true,
+
+                ClockSkew = TimeSpan.Zero
+            };
+
+            var handler = new JsonWebTokenHandler();
+            var result = await handler.ValidateTokenAsync(token, validationParameters);
+            return result.IsValid;
+        }
+}
--- RestaurantReservation/RestaurantReservation.API/Program.cs
+++ RestaurantReservation/RestaurantReservation.API/Program.cs
@@ -1,74 +1,143 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.IdentityModel.Tokens;
+using RestaurantReservation.API.Auth;
 using RestaurantReservation.Db.Models;
 using RestaurantReservation.Db.Repositories;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace RestaurantReservation.API;
 
-builder.Services.AddOpenApi();
-
-var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-var api = app.MapGroup("/api");
-var reservationApi = api.MapGroup("/reservations");
-var employeeApi = api.MapGroup("/employees");
-
-reservationApi.MapGet("/", async () => await ReservationRepository.GetAll());
-
-reservationApi.MapGet("/{id:int}", async (int id) =>
+internal static class MainClass
 {
-    var reservation = await ReservationRepository.GetById(id);
-    return reservation == null ? Results.NotFound() : Results.Ok(reservation);
-});
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-reservationApi.MapPost("/", async (Reservation reservation) =>
-{
-    var res = await ReservationRepository.Create(reservation);
-    return Results.Ok(res);
-});
+        builder.Services.AddOpenApi();
 
-reservationApi.MapPut("/{id:int}", async (int id, Reservation reservation) =>
-{
-    var res = await ReservationRepository.Update(id, reservation);
-    return Results.Ok(res);
-});
+        
+        builder.Services.AddScoped<JwtTokenGenerator>();
+        builder.Services.Configure<JwtConfig>(
+            builder.Configuration.GetSection("JwtConfig")
+        );
 
-reservationApi.MapGet("/customer/{customerId:int}", async (int customerId) =>
-{
-    var res = await ReservationRepository.GetReservationsByCustomer(customerId);
-    return Results.Ok(res);
-});
+        var jwtConfig = builder.Configuration
+                            .GetSection("JwtConfig")
+                            .Get<JwtConfig>()
+                        ?? throw new InvalidOperationException();
+        
+        var signingKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(jwtConfig.Key)
+        );
+        builder.Services
+            .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = signingKey,
 
-reservationApi.MapGet("/{reservationId:int}/orders", async (int reservationId) =>
-{
-    var res = await ReservationRepository.ListOrdersAndMenuItems(reservationId);
-    return Results.Ok(res);
-});
+                ValidateIssuer = true,
+                ValidIssuer = jwtConfig.Issuer,
 
-reservationApi.MapGet("/{reservationId:int}/menuItems", async (int reservationId) =>
-{
-    var res = await ReservationRepository.ListOrderedAndMenuItems(reservationId);
-    return Results.Ok(res);
-});
+                ValidateAudience = true,
+                ValidAudience = jwtConfig.Audience,
 
-employeeApi.MapGet("/managers", async () =>
-{
-    var managers = await EmployeeRepository.ListManagers();
-    return Results.Ok(managers);
-});
+                ValidateLifetime = true,
 
-employeeApi.MapGet("/{employeeId:int}", async (int employeeId) =>
-{
-    var res = await EmployeeRepository.CalculateAverageOrderAmount(employeeId);
-    return Results.Ok(res);
-});
+                ClockSkew = TimeSpan.Zero
+            });
+        builder.Services.AddAuthorization();
+        var app = builder.Build();
+        
+        app.UseAuthentication();
+        app.UseAuthorization();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
+        app.MapGet("/", () => "Hello World!");
+        var api = app.MapGroup("/api");
+        var reservationApi = api.MapGroup("/reservations");
+        var employeeApi = api.MapGroup("/employees");
+
+        reservationApi.MapGet("/", async () => await ReservationRepository.GetAll());
+
+        reservationApi.MapGet("/{id:int}", async (int id) =>
+        {
+            var reservation = await ReservationRepository.GetById(id);
+            return reservation == null ? Results.NotFound() : Results.Ok(reservation);
+        });
+
+        reservationApi.MapPost("/", async (Reservation reservation) =>
+        {
+            var res = await ReservationRepository.Create(reservation);
+            return Results.Ok(res);
+        });
+
+        reservationApi.MapPut("/{id:int}", async (int id, Reservation reservation) =>
+        {
+            var res = await ReservationRepository.Update(id, reservation);
+            return Results.Ok(res);
+        });
+
+        reservationApi.MapGet("/customer/{customerId:int}", async (int customerId) =>
+        {
+            var res = await ReservationRepository.GetReservationsByCustomer(customerId);
+            return Results.Ok(res);
+        });
+
+        reservationApi.MapGet("/{reservationId:int}/orders", async (int reservationId) =>
+        {
+            var res = await ReservationRepository.ListOrdersAndMenuItems(reservationId);
+            return Results.Ok(res);
+        });
+
+        reservationApi.MapGet("/{reservationId:int}/menuItems", async (int reservationId) =>
+        {
+            var res = await ReservationRepository.ListOrderedAndMenuItems(reservationId);
+            return Results.Ok(res);
+        });
+
+        employeeApi.MapGet("/managers", async () =>
+        {
+            var managers = await EmployeeRepository.ListManagers();
+            return Results.Ok(managers);
+        });
+
+        employeeApi.MapGet("/{employeeId:int}", async (int employeeId) =>
+        {
+            var res = await EmployeeRepository.CalculateAverageOrderAmount(employeeId);
+            return Results.Ok(res);
+        });
+        
+        app.MapPost("/login", (LoginRequest request, JwtTokenGenerator tokenGenerator) =>
+        {
+            if (request.Username != "suha" || request.Password != "123")
+            {
+                return Results.Unauthorized();
+            }
+
+            var token = tokenGenerator.GenerateToken(
+                request.Username,
+                request.Password
+            );
+
+            return Results.Ok(new
+            {
+                token
+            });
+        });
+        
+        app.MapGet("/welcome", () => "Welcome! You are authorized.").RequireAuthorization();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.Run();
+    }
 }
 
-app.UseHttpsRedirection();
-
-app.Run();
-
+public record LoginRequest(string Username, string Password);
